f41ac863cadafbbcfec2781e7b910ac80bae2a8d
DiegoGonzalez2807 <diego.gonzalez-g@mail.escuelaing.edu.co>
2022-02-12 19:50:38 -0500

    Primer punto de etiquetas

--- src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
+++ src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
@@ -13,6 +13,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 /**
@@ -23,7 +24,8 @@ import org.springframework.stereotype.Service;
 public class BlueprintsServices {
    
     @Autowired
-    BlueprintsPersistence bpp=null;
+    @Qualifier("Memory")
+    BlueprintsPersistence bpp;
     
     public void addNewBlueprint(Blueprint bp){
         
