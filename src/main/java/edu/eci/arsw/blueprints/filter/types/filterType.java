62d8bde2703bd504d1995eb585bdd1379071a60a
Critian Castellanos <cristian.castellanos@mail.escuelaing.edu.co>
2022-02-15 23:54:55 -0500

    Arreglos finales

--- src/main/java/edu/eci/arsw/blueprints/filter/types/filterType.java
+++ src/main/java/edu/eci/arsw/blueprints/filter/types/filterType.java
@@ -8,7 +8,26 @@ import java.util.Set;
 
 public interface filterType {
 
+    /**
+     * Funcion sobreescrita para que filtre los puntos de un blueprint
+     * @param bp
+     * @throws BlueprintNotFoundException
+     */
     public void filterBlueprint(Blueprint bp) throws BlueprintNotFoundException;
+
+    /**
+     * Funcion sobreescrita para filtrar  un arreglo de blueprints
+     * @param blueprints
+     * @throws BlueprintPersistenceException
+     * @throws BlueprintNotFoundException
+     */
     public void filterBlueprints(Set<Blueprint> blueprints) throws BlueprintPersistenceException, BlueprintNotFoundException;
+
+    /**
+     * Funcion sobreescrita para filtrar un arreglo de blueprints pero solo del autor escogido
+     * @param author
+     * @param blueprints
+     * @throws BlueprintNotFoundException
+     */
     public void filterPrintsByAuthor(String author, Set<Blueprint> blueprints) throws BlueprintNotFoundException;
 }
